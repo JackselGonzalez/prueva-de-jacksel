8408d2ab1cb2a2baac6dae50bbd9109994b2e2e0
JackselGonzalez <haczelgonzale19@gmail.com>
2021-03-11 22:17:40 +0100

    hola humanos

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+//Esta es la primera practica
+$nombre = "Jacksel";
+$apellido = "Gonzalez";
+$edad = "22";
+
+echo("My nombre es " . $nombre . " " . $apellido . " y tengo de edad " . $edad . " años " . "<br>"  );
+
+ ?>
+
+
+
+
+<?php
+/*
+$nombre = "jacksel";
+
+echo wordwrap($nombre, 10000 , "<br>")."<p />";
+echo wordwrap( $nombre, 10000, "<br/>", true )."<p />";
+
+echo "<pre>";
+     echo wordwrap( $nombre, 10000, "\n" );
+    echo "</pre>";
+
+*/
+?>
